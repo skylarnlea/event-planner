d99b4943bfbc2967778500a162b1d2e36a9edf63
Skylar Lea <skyniclea@gmail.com>
2025-03-19 12:18:14 -0500

    server.js updated with routes

--- server/server.js
+++ server/server.js
@@ -6,5 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const eventRoutes = require('./routes/eventRoutes');
+const authRoutes = require('./routes/authRoutes');
+const rsvpRoutes = require('./routes/rsvpRoutes');
+
+app.use('/auth', authRoutes);
+app.use('/events', eventRoutes);
+app.use('/rsvp', rsvpRoutes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
